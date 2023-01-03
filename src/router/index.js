7853034cd624c1bebb7dcad08ad5a2a0fe75fc7f
tomit4 <mosssap@gmail.com>
2023-01-02 19:09:54 -0800

    :pencil2: Added home path to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import {createRouter, createWebHistory} from "vue-router"
+import Home from "../components/Home.vue"
 import TestView from "../views/TestView.vue"
 import NotFoundView from "../views/404View.vue"
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        {
+            path: "/",
+            name: "home",
+            component: Home
+        },
         {
             path: "/test",
             name: "test",

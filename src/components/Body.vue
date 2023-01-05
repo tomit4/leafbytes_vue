c32e4e244f29df855eed0f393238c862acd26bb7
tomit4 <mosssap@gmail.com>
2023-01-04 16:51:59 -0800

    :recycle: Refactored css colors to variables in Body.vue

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -3,8 +3,6 @@
     import {RouterView} from "vue-router"
     import { delay } from '../utilities/utils.js'
 
-    /* import TestView from "../views/TestView.vue" */
-
     const showArticle = ref(false)
 
     onMounted(async () => {
@@ -23,21 +21,16 @@
 
 <style scoped>
     .article {
-        /* visibility: hidden; */
         top: 50vh;
         margin: -42vh auto auto auto;
         left: 0;
         right: 0;
         position: fixed;
-        background-image: linear-gradient(
-            280deg,
-            rgba(98, 165, 191, 0.2) 0%,
-            rgba(42, 114, 150, 0.7) 120%
-        );
+        background-image: linear-gradient( 280deg, var(--bg-light-blue-20), var(--bg-dark-blue-70) 120% );
         width: 98vw;
         max-width: 1040px;
         height: 83.5vh;
-        border: 6px solid #004b77;
+        border: 6px solid var(--dark-blue-90);
         border-radius: 5px;
         overflow-y: scroll;
         overscroll-behavior: none;
@@ -59,6 +52,6 @@
         max-width: 840px;
         margin: 0.5rem auto;
         border-radius: 5px;
-        background-color: rgba(200, 237, 255, 0.5);
+        background-color: var(--light-blue-50);
     }
 </style>

fd84d3cf84b3eb454adc77c6cbf3e523203329f5
tomit4 <mosssap@gmail.com>
2023-01-04 23:22:01 -0800

    :pencil2: Refactored toggletree to be reusable

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -2,4 +2,34 @@ function delay(ms) {
     return new Promise(res => setTimeout(res, ms))
 }
 
-export { delay }
+async function toggleTree(vertEl, subjEl, horizEl, className) {
+    if (vertEl.value.classList.contains(`${className}-tree-vert-show`)) {
+        subjEl.value.forEach((el) => {
+            el.classList.remove(`${className}-tree-subject-show`)
+            el.classList.add(`${className}-tree-subject-hidden`)
+        })
+        await delay(1000)
+        horizEl.value.forEach((el) => {
+            el.classList.remove(`${className}-tree-horiz-show`)
+            el.classList.add(`${className}-tree-horiz-hidden`)
+        })
+        await delay(1000)
+        vertEl.value.classList.remove("${className}-tree-vert-show")
+        vertEl.value.classList.add("${className}-tree-vert-hidden")
+    } else {
+        vertEl.value.classList.remove("${className}-tree-vert-hidden")
+        vertEl.value.classList.add("${className}-tree-vert-show")
+        await delay(1200)
+        horizEl.value.forEach((el) => {
+            el.classList.add(`${className}-tree-horiz-show`)
+            el.classList.remove(`${className}-tree-horiz-hidden`)
+        })
+        await delay(1000)
+        subjEl.value.forEach((el) => {
+            el.classList.remove(`${className}-tree-subject-hidden`)
+            el.classList.add(`${className}-tree-subject-show`)
+        })
+    }
+}
+
+export { delay, toggleTree }

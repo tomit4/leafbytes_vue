2541a97ba3c3894d5e71904e049bf6cf00191a1a
tomit4 <mosssap@gmail.com>
2023-01-05 01:10:34 -0800

    :construction: Setting up for reuse

--- src/views/views_components/Portfolio.vue
+++ src/views/views_components/Portfolio.vue
@@ -10,32 +10,32 @@
 
     async function toggleTree(className) {
         /* console.log(`you clicked on the ${className} tree`) */
-        if (vertEl.value.classList.contains("portfolio-tree-vert-show")) {
+        if (vertEl.value.classList.contains(`${className}-tree-vert-show`)) {
             subjEl.value.forEach((el) => {
-                el.classList.remove('portfolio-tree-subject-show')
-                el.classList.add('portfolio-tree-subject-hidden')
+                el.classList.remove(`${className}-tree-subject-show`)
+                el.classList.add(`${className}-tree-subject-hidden`)
             })
             await delay(1000)
             horizEl.value.forEach((el) => {
-                el.classList.remove('portfolio-tree-horiz-show')
-                el.classList.add('portfolio-tree-horiz-hidden')
+                el.classList.remove(`${className}-tree-horiz-show`)
+                el.classList.add(`${className}-tree-horiz-hidden`)
             })
             await delay(1000)
-            vertEl.value.classList.remove("portfolio-tree-vert-show")
-            vertEl.value.classList.add("portfolio-tree-vert-hidden")
+            vertEl.value.classList.remove(`${className}-tree-vert-show`)
+            vertEl.value.classList.add(`${className}-tree-vert-hidden`)
         }
         else {
-            vertEl.value.classList.remove("portfolio-tree-vert-hidden")
-            vertEl.value.classList.add("portfolio-tree-vert-show")
+            vertEl.value.classList.remove(`${className}-tree-vert-hidden`)
+            vertEl.value.classList.add(`${className}-tree-vert-show`)
             await delay(1200)
             horizEl.value.forEach((el) => {
-                el.classList.add('portfolio-tree-horiz-show')
-                el.classList.remove('portfolio-tree-horiz-hidden')
+                el.classList.add(`${className}-tree-horiz-show`)
+                el.classList.remove(`${className}-tree-horiz-hidden`)
             })
             await delay(1000)
             subjEl.value.forEach((el) => {
-                el.classList.remove('portfolio-tree-subject-hidden')
-                el.classList.add('portfolio-tree-subject-show')
+                el.classList.remove(`${className}-tree-subject-hidden`)
+                el.classList.add(`${className}-tree-subject-show`)
             })
         }
     }

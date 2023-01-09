36514540f557d7a4343cc38beff90f83d8a94af5
tomit4 <mosssap@gmail.com>
2023-01-08 19:28:31 -0800

    :recycle: Refactored color values in contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -27,9 +27,9 @@
         align-items: center;
         text-align: center;
         position: fixed;
-        border: 6px solid #004b77;
+        border: 6px solid var(--dark-blue);
         border-radius: 5px;
-        background-image: linear-gradient(280deg, #62a5bf 0%, #2a7296 100%);
+        background-image: linear-gradient(280deg, var(--bg-light-blue) 0%, var(--bg-dark-blue) 100%);
         height: 9rem;
         width: 80vw;
         max-width: 400px;
@@ -49,7 +49,7 @@
         font-size: clamp(110%, 1rem, 120%);
         width: 20rem;
         text-align: center;
-        text-shadow: 5px 4px 0px rgba(0, 75, 119, 0.7);
+        text-shadow: 5px 4px 0px var(--dark-blue-70);
         text-decoration: none;
         color: white;
         z-index: 2;

968a14a5f7b624ad5d8e7685d9ecb705dba1ffd9
tomit4 <mosssap@gmail.com>
2023-01-17 19:49:20 -0800

    :construction: Starting intro home screen dev

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
-        <h1>HomeView Test</h1>
+        <div class="container">
+            <div class="square"></div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        top: 35vh;
+    }
+
+    .square {
+        width: 8rem;
+        height: 8rem;
+        border: 6px solid var(--dark-blue-70);
+        background-color: var(--light-blue-50);
+        border-radius: 5px;
+    }
+</style>
